d1ab2c03afb00f2f736a3657bee825a53a20013a
RONCIN Thomas <thomas.roncin@soprasteria.com>
2019-04-12 09:54:38 +0200

    fix(tsconfig): fix reload issue

--- src/react/business/AppLayout.tsx
+++ src/react/business/AppLayout.tsx
@@ -3,7 +3,13 @@ import * as React from "react";
 import AppHeader from "./AppHeader";
 
 const { Content, Footer } = Layout;
-const AppLayout = ({ pageName, pageContent }) => (
+const AppLayout = ({
+  pageName,
+  pageContent
+}: {
+  pageName: string;
+  pageContent: any;
+}) => (
   <Layout style={{ height: "100%" }}>
     <AppHeader current={pageName} size={64} />
     <Content>{pageContent}</Content>
