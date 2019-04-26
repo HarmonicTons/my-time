8064333b419fb54e372cc22d441c23d37bdd55dc
RONCIN Thomas <thomas.roncin@soprasteria.com>
2019-04-26 17:43:38 +0200

    WIP report

--- src/react/views/Report.tsx
+++ src/react/views/Report.tsx
@@ -1,15 +1,69 @@
 import * as React from "react";
+import { connect } from "react-redux";
+import { IActivity } from "src/interfaces/IActivity";
+import { IUser } from "src/interfaces/IUser";
+import { IUserState } from "src/redux/user/reducer";
+import { list } from "../../services/activity";
 import AppLayout from "../business/AppLayout";
 
-const Report = () => {
+const mapStateToProps = ({ user: userState }: { user: IUserState }) => {
+  const { user } = userState;
+  return {
+    user
+  };
+};
+
+interface IAsyncState<T> {
+  loading: boolean;
+  error: Error | null;
+  data?: T;
+}
+
+const Report = ({ user }: { user: IUser }) => {
+  const [activityFetching, setActivityFetching] = React.useState<
+    IAsyncState<IActivity[]>
+  >({
+    loading: false,
+    error: null,
+    data: []
+  });
+
+  const refresh = async () => {
+    setActivityFetching({
+      loading: true,
+      error: null,
+      data: activityFetching.data
+    });
+    try {
+      const res = await list(user.id);
+      setActivityFetching({
+        loading: false,
+        error: null,
+        data: res
+      });
+    } catch (error) {
+      setActivityFetching({
+        loading: false,
+        error,
+        data: activityFetching.data
+      });
+    }
+  };
+
+  React.useEffect(() => {
+    if (user) {
+      refresh();
+    }
+  }, [user]);
+
   return (
     <AppLayout
       pageName="report"
       pageContent={(() => (
-        <>report</>
+        <>{JSON.stringify(activityFetching.data)}</>
       ))()}
     />
   );
 };
 
-export default Report;
+export default connect(mapStateToProps)(Report);
